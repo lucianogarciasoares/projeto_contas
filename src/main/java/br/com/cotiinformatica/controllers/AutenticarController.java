6e067b88dae6986c8c5ceecf693d0cfbde8a85dd
lucianojavadev <luciano.garcia.s1989@gmail.com>
2023-05-09 21:59:53 -0300

    feat: criar as paginas de autenticar, criar usuário e recuperar senha

---
+++ src/main/java/br/com/cotiinformatica/controllers/AutenticarController.java
@@ -0,0 +1,17 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AutenticarController {
+
+	@RequestMapping(value = "/") // página raiz do projeto(inicial)
+	public ModelAndView autenticar() {
+
+		// WEB-INF/views/autenticar.jsp
+		ModelAndView modelAndView = new ModelAndView("autenticar");
+		return modelAndView;
+	}
+}
